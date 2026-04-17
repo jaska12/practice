0629d3783e8537446e91116a490341053635c76e
jaska <farisgithub02@gmail.com>
2026-04-17 17:01:47 +0900

    fix: learn key word & defolt argument

--- function.py
+++ function.py
@@ -34,3 +34,30 @@ print("result1:", result1)
 result2 = greet('jack')
 print("result2:", result2)
 # jacek
+
+
+print("===== Keyword & default arguments =====")
+
+# DEFINE - Funksiyani yaratish
+# age=22 bu "Default argument" hisoblanadi.
+# Agar funksiya chaqirilganda yosh berilmasa, avtomatik 22 olinadi.
+
+
+def give_greet(name, age=22):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old!"
+
+# CALL - Funksiyani chaqirish
+
+
+# 1. Keyword arguments (Kalit so'zli argumentlar)
+# Bu usulda biz argumentlarni nomi bilan uzatamiz: name="Justin", age=28
+# Bunda argumentlar ketma-ketligini almashtirib yozsa ham bo'ladi.
+result3 = give_greet(name="jack", age=28)
+print("result3:", result3)
+
+# 2. Default value ishlatilishi
+# Biz faqat "John" ismini berdik, age (yosh) uchun qiymat bermadik.
+# Shunda funksiya o'zining tepada belgilangan age=22 qiymatini ishlatadi.
+result4 = give_greet("jack")
+print("result4:", result4)
