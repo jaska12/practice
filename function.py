2e0160e2295b7dbef36df768aae246b9e48a8de3
jaska <farisgithub02@gmail.com>
2026-04-17 16:52:57 +0900

    feat: learn boolean in ptyhon

---
+++ function.py
@@ -0,0 +1,35 @@
+'''
+FUNCTIONS
+(1) DEFINE vs CALL (Aniqlash va Chaqirish)
+(2) Parametr vs Argument
+(3) Keyword & default arguments
+(4) Scope (Ko'rinish sohasi)
+'''
+
+print("===== DEFINE (PARAMETER) vs CALL (ARGUMENT)=====")
+# build in function (tayyor funksiyalar) > print() type()
+# Function - reusable block of code! (Funksiya - qayta ishlatiladigan kod bloki)
+# Java-dagi {} qavslar o'rniga Python-da surish (indentation) ishlatiladi!
+
+# 1. DEFINE (Funksiyani qurish/yaratish)   PARAMETER
+
+
+def greet(a):
+    # Bu funksiya shunchaki ma'lumotni ekranga chiqaradi (print)
+    print(f"How do you do, {a}")
+
+
+def greeting(b):
+    # Bu funksiya esa natijani qaytaradi (return)
+    print("greeting is executed")
+    return f"Hi {b}"
+
+
+# 2. CALL (Funksiyani ishga tushirish/chaqirish)excute                 ARGUMENT
+# greet() funksiyasini chaqiramiz. U ichida 'return' bo'lmagani uchun None qaytaradi.
+result1 = greet('Jasurbek')
+# result1: None chiqadi, chunki greet funksiyasida return yo'q
+print("result1:", result1)
+
+result2 = greet('jack')
+print("result2:", result2)
