5a1cc109aab8d55bae5ab158762da498f6ea4641
jaska <farisgithub02@gmail.com>
2026-04-20 01:02:27 +0900

    feat :  learn class and static property

--- class.about.py
+++ class.about.py
@@ -5,31 +5,36 @@
 '''
 
 print("===== What is class =====")
-# class - blueprint for object creation! (obyekt yaratish uchun qolip)
-# structure > state constructor method (tuzilishi: holat, konstruktor va metod)
+# class - blueprint for object creation!
+# structure > state constructor method
 
 
 class Person():
-    # state (holat yoki klass xususiyati)
+    # static state (klassga tegishli xususiyat)
     message = "class state property"
 
-    # constructor (konstruktor)
+    # constructor (maxsus metod)
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self.name = name  # ordinary property
+        self.age = age   # ordinary property
 
-    # method (metod - klass ichidagi funksiya)
+    # ordinary method (obyektga tegishli metod)
     def introduce(self):
         print(f"{self.name} says: How do you do!")
 
     def say_age(self):
         print(f"{self.name} says I am {self.age}!")
 
+    # static method (klassga tegishli metod)
+    @staticmethod
+    def explain():
+        print("class: static method property executed!")
+
 
 # Obyektlarni yaratish (Instances)
-person1 = Person("Justin", 25)
-person2 = Person("Martin", 35)
-person3 = Person("John", 22)
+person1 = Person("Jeck", 22)
+person2 = Person("Jasurbek", 35)
+person3 = Person("Javlon", 22)
 
 # Oddiy xususiyatni chiqarish
 print("person1.name:", person1.name)
@@ -37,3 +42,12 @@ print("person1.name:", person1.name)
 # Metodlarni ishga tushirish
 person1.introduce()
 person2.say_age()
+
+print("===== ordinary vs static properties =====")
+
+# static state murojaat
+new_message = Person.message
+print("new_message:", new_message)
+
+# static method murojaat
+Person.explain()
