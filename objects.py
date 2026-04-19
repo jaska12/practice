dd2540ccc67b85455e7a10f76ed2f5db765b493c
jaska <farisgithub02@gmail.com>
2026-04-20 00:30:58 +0900

    fix: handle all kind of errors

--- objects.py
+++ objects.py
@@ -32,3 +32,26 @@ print("result1:", result1)
 
 result2 = ceil(98.7)       # to‘g‘ridan-to‘g‘ri ceil ishlatildi
 print("result2:", result2)
+
+print("===== Error handling system =====")
+car_dict = dict(name="Tayota", year=2026, electric=True)
+
+try:
+    print("passed here")
+    # 1-xato: Lug'atda nuqta orqali murojaat (AttributeError)
+    a = car_dict.speed
+    # 2-xato: Mavjud bo'lmagan kalit (KeyError)
+    result = car_dict["origin"]
+    print("result:", result)
+
+except Exception as err:
+    # Xato yuz berganda shu qism ishlaydi
+    print("General Error:", err)
+
+else:
+    # Xato bo'lmasa shu qism ishlaydi
+    print("Executed successfully without errors")
+
+finally:
+    # Har qanday holatda ham ishlaydi
+    print("Final closing logic")
