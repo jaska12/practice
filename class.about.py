d0699657e75a38ccb3c068d19a927176b26d0fda
jaska <farisgithub02@gmail.com>
2026-04-20 01:38:00 +0900

    fix : python special/magic

--- class.about.py
+++ class.about.py
@@ -1,7 +1,7 @@
 ''' CLASS
 (1) What is class
 (2) ordinary vs static properties
-(3) special methods
+(3) special methods/ magic method ham deb nomlanadi 
 '''
 
 print("===== What is class =====")
@@ -51,3 +51,48 @@ print("new_message:", new_message)
 
 # static method murojaat
 Person.explain()
+
+print("--- special/magic methods -----")
+# Python's most common special methods are below:
+# __init__ __name__ __str__ __call__ __getitem__ __add__ __len__ ...
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("* __new__ *")
+        # Diqqat: rasmda super().__new__(cls) bo'lishi kerak
+        return super().__init__.__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+        print("Object called as function!")
+        return True
+
+
+# Obyektlar yaratish va ishlatish
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("------")
+your_car = Car("Tayota", 2026)
+print(your_car)  # __str__ metodi ishlaydi
+response = your_car()       # __call__ metodi ishlaydi
+print("response:", response)
