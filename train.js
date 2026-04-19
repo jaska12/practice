5c3f531f17112a2682ba2af515eab0eabf6bbb78
jaska <farisgithub02@gmail.com>
2026-04-20 01:46:37 +0900

    B-TASK

--- train.js
+++ train.js
@@ -2,7 +2,7 @@
 Savol: Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi 
 letterni ikkinchi parametrdagi so'zdan qatnashgan sonini return qilishi kerak bo'ladi.
 MASALAN: countLetter("e", "engineer") 3ni return qiladi.
-*/
+
 
 //  Masalaning yechimi:
 function countLetter(letter, word) {
@@ -18,10 +18,30 @@ function countLetter(letter, word) {
 
     return count;
 }
+*/
 
-// "Full Stack Developer" so'zida nechta 'l' harfi borligini tekshiramiz
-const res = countLetter("l", "Full Stack Developer");
-console.log(res);
+/*
+B-TASK:
+Shunday function tuzing, u 1ta string parametrga ega bolsin, 
+hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 
-const res2 = countLetter("e", "Full Stack Developer");
-console.log(res2); 
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+
+
+//  Masalaning yechimi:
+function countDigits(text) {
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        // Char raqam ekanligini tekshirish (bo'shliq bo'lmasligi va raqam bo'lishi sharti)
+        if (text[i] >= '0' && text[i] <= '9') {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log("Digits count:", result);
