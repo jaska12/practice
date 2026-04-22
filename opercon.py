b42e4c43a904ee6a906c07e7f343de13a55ccbbe
jaska <farisgithub02@gmail.com>
2026-04-22 18:19:00 +0900

    feat:  lear Operators vs  Logic operators

--- opercon.py
+++ opercon.py
@@ -34,3 +34,51 @@ print("c == d", c == d)  # True (qiymatlari bir xil)
 print("c is d", c is d)  # False (xotirada boshqa-boshqa joyda)
 print("c is e", c is e)  # True (e o'zgaruvchisi c ning manziliga bog'langan)
 print(id(c), id(d), id(e))  # Ularning xotiradagi ID manzillari
+print("===== Condition =====")
+x = 15
+
+if x > 50:
+    print("Case A")
+elif x > 10:
+    print("Case B")
+else:
+    print("Case C")
+
+print("===== Logical Operators =====")
+age = 21
+
+# person = None
+# if age > 16:
+#     person = "adult"
+# else:
+#     person = "minor"
+# Ternary operator - bir qatorli shart ifodasi
+age = 20
+person = "adult" if age > 18 else "minor"
+# age 18 dan katta bo'lsa "adult", kichik bo'lsa "minor" qiymat beradi
+print("person:", person)  # Natija: person: adult
+
+print("-----")
+
+# Boolean (True/False) o'zgaruvchilar
+is_student = True   # Talaba - ha
+is_admin = False    # Admin - yo'q
+is_guest = True     # Mehmon - ha
+is_parent = False   # Ota-ona - yo'q
+
+# Shartli tekshiruv
+if not is_student:
+    # Agar talaba EMAS bo'lsa
+    print("Wellcome here, do you want to be student!")
+
+elif is_admin:
+    # Agar admin bo'lsa
+    print("Please go to this office!")
+
+elif is_guest or is_parent:
+    # Agar mehmon YOKI ota-ona bo'lsa
+    print("Waiting room is over there!")
+
+else:
+    # Yuqoridagi shartlarning hech biri to'g'ri kelmasa
+    print("Other cases")
