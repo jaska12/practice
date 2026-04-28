f0e9e9e52431944a11f3cdd1966e2838012f215e
jaska <farisgithub02@gmail.com>
2026-04-28 22:11:19 +0900

    feat: set & dictionary comprehension

--- comprehension.py
+++ comprehension.py
@@ -32,3 +32,15 @@ cars = [
 # c version - shartga mos elementlarni oladi (tezligi 80 dan katta)
 list_cars = [car[0] for car in cars if car[1] > 80]
 print("list_cars:", list_cars)  # ["Tayota", "Audi", "BMW"]
+print("===== set and dictionary comprehension =====")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+set_numbs = {*numbs}              # takroriy elementlarni o'chiradi
+print("set_numbs:", set_numbs)    # {1, 4, 5, 20} - faqat unikal qiymatlar
+
+
+# dict comprehension - b version
+# person[0] = ism (key), person[1] = yosh (value)
+dict_people = {person[0]: person[1] for person in people}
+print("dict_people:", dict_people)  # {"Robert": 20, "Steve": 19, "Joseph": 25}
+
+# (<expression> for item in iterable) generic
